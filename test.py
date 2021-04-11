a8bd249493a16b86319888f0e08b02a6c183e038
John Cruzado <johnrcruzado@gmail.com>
2021-04-11 17:07:29 +0800

    assert and fix
    fix on test_dataset_classification_fake

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # pylint: disable=C0321,C0103,C0301,E1305,E1121,C0302,C0330,C0111,W0613,W0611,R1705
 # -*- coding: utf-8 -*-
-import os, sys, time, datetime,inspect
+import os, sys, time, datetime,inspect,random
 
 def log(*s):
     print(*s, flush=True)
@@ -23,6 +23,26 @@ def test1():
      myfun    = locals()[t]
      df, pars = myfun(100)
      log(t, "\n", df, pars)
+     
+   
+     nrow = random.randint(0,20)*100
+     df, pars = test_dataset_classification_fake(nrow)
+     assert len(df.index) == nrow, 'mismatch test_dataset_classification_fake row data '+str(len(df.index)) +" vs "+str(nrow)
+
+   
+     nrow = random.randint(0,20)*100
+     df, pars =  test_dataset_regression_fake(nrow)
+     assert len(df.index) == nrow, 'mismatch test_dataset_regression_fake row data'+str(len(df.index)) +"vs"+str(nrow)
+
+   
+     nrow = random.randint(0,20)*100
+     df, pars = test_dataset_classifier_covtype(nrow)
+     assert len(df.index) == nrow, 'mismatch test_dataset_classifier_covtype row data'+str(len(df.index)) +"vs"+str(nrow)
+
+    
+     nrow = random.randint(0,20)*100
+     df, pars = test_dataset_classification_petfinder(nrow)
+     assert len(df.index) == nrow, 'mismatch test_dataset_classification_petfinder row data'+str(len(df.index)) +"vs"+str(nrow)
 
 
 
